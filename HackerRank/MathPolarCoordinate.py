24e68438eda9e7cad4e79e5d924d50cfb63eeca7
Taufiq Hidayat <117139624+taufiqxy@users.noreply.github.com>
2023-06-23 07:23:34 +0700

    Create MathPolarCoordinate.py

---
+++ HackerRank/MathPolarCoordinate.py
@@ -0,0 +1,10 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+# import
+from cmath import phase
+
+#read input
+comNum = complex(input())
+
+print(abs(comNum))
+print(phase(comNum))
